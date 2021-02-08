a1ccc57cbb32012f2a8c425ef9ad448e89da3c13
Nematjon <frank.sport.93@gmail.com>
2021-02-08 13:31:57 +0300

    Added get and post route to /api/books.

--- routes/api.js
+++ routes/api.js
@@ -8,17 +8,52 @@
 
 'use strict';
 
+const { ObjectID } = require('mongodb');
+
+const filterer = (arr, obj) => {
+    arr.filter(a => !!a[1]).map(a => obj[a[0]] = a[1])
+}
+
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    
+          const collection = myDataBase.collection("books");
+
+          collection.find({}).toArray((err, data) => {
+              if(err) {
+                  return res.json({"error": "Server error, please try later"})
+              } else if(!!data) {
+                  return res.status(200).json(data);
+              }
+          });
     })
     
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+    
+        if(!title) {
+            return res.send("missing required field title");
+        } else {
+            const collection = myDataBase.collection("books");
+
+            collection.insertOne({
+                "title":title,
+                "commentcount":0,
+                "__v":1,
+                "comments":[]
+            }, (err, data) => {
+                if(err) {
+                    return res.json({"error": "Server error, please try later"})
+                } else if(data) {
+                    return res.status(200).json(data.ops[0]);
+                }
+            });
+        }
     })
     
     .delete(function(req, res){
